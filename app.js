8e1ebe30c3669d887a4305fe43e266e5d3f80f0b
Juan Manuel Cantero Diego <juanmanuelcantero@gmail.com>
2020-06-26 12:25:53 +0200

    created the function to start and restart the game and the keycodes

--- app.js
+++ app.js
@@ -13,6 +13,48 @@ document.addEventListener('DOMContentLoader', () => {
     let intervalTime = 0
     let interval = 0
 
+    //to start, and restart the game
+    function startGame() {
+        currentSnake.forEach(index => squares[index].classList.remove('snake'))
+        squares[appleIndex].classList.remove('apple')
+        clearInterval(interval)
+        score = 0
+        //randomApple()
+        direction = 1
+        scoreDisplay.innerText = score
+        intervalTime = 1000
+        currentSnake = [2,1,0]
+        currentIndex = 0
+        currentSnake.forEach(index => squares[index].classList.add('snake'))
+        interval = setInterval(moveOutcomes, intervalTime)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
     //assing function to the keycodes
-    
+    function control(e) {
+        squares[currentIndex].classList.remove('snake')
+
+        if(e.keyCode === 39) {
+            direction = 1 //if we press the right arrow in our keyboard pur snake will move to the right
+        } else if (e.keyCode === 38) {
+            direction = -width //if we press the up arrow, the snake will move up
+        } else if (e.keyCode === 37) {
+            direction = -1 //if we press left, the snake will move left
+        } else if (e.keyCode === 40) {
+            direction = +width //if we press down, the snake will move down
+        }
+    }
+
+    document.addEventListener('keyup', control)
 })
